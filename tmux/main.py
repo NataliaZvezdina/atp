5689bd92754e4a57d2bee2ff528e93623edb40a4
NataliaZvezdina <natalja.zvezdina@yandex.by>
2022-09-24 23:05:47 +0300

    add click options to functions

--- tmux/main.py
+++ tmux/main.py
@@ -7,9 +7,12 @@ def cli_1():
 
 
 @cli_1.command()
-def start():
+@click.option('-n', '--number', required=True, type=click.IntRange(min=1),
+              help='Provide number of users jupyter notebooks that should be run')
+def start(number):
     """Command on cli_1"""
     click.echo('Command start on cli_1')
+    click.echo(f'start running {number} envs')
 
 
 @click.group()
@@ -18,9 +21,14 @@ def cli_2():
 
 
 @cli_2.command()
-def stop():
+@click.option('-s', '--session-name', required=True, type=click.IntRange(min=0),
+              help='Tmux-session number where environments are running')
+@click.option('-n', '--number', required=True, type=click.IntRange(min=0),
+              help='The sequence number of environment that can be killed')
+def stop(session_name, number):
     """Command on cli_2"""
     click.echo('Command stop on cli_2')
+    click.echo(f'stop env with number {number} at session {session_name}')
 
 
 @click.group()
@@ -29,9 +37,12 @@ def cli_3():
 
 
 @cli_3.command()
-def stop_all():
+@click.option('-s', '--session-name', required=True, type=click.IntRange(min=0),
+              help='Tmux-session number where environments are running')
+def stop_all(session_name):
     """Command on cli_3"""
     click.echo('Command stop_all on cli_3')
+    click.echo(f'stop all environments at session {session_name}')
 
 
 cli = click.CommandCollection(sources=[cli_1, cli_2, cli_3])
