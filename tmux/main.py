4765e38e14050ea5b1816420904ef1098a52000e
NataliaZvezdina <natalja.zvezdina@yandex.by>
2022-09-26 09:09:27 +0300

    edit main.py
    changes:
    -add os lib for generating token
    -extract functionality for running envs to func run_commands()
    -add implementation of stop(), stop_all functions

--- tmux/main.py
+++ tmux/main.py
@@ -1,5 +1,6 @@
 import click
 import libtmux
+import os
 
 
 @click.group()
@@ -10,18 +11,19 @@ def cli_1():
 @cli_1.command()
 @click.option('-n', '--number', required=True, type=click.IntRange(min=1),
               help='Provide number of users jupyter notebooks that should be run')
-def start(number):
+def start(number, base_dir='./'):
     """Command on cli_1"""
     click.echo('Command start on cli_1')
     click.echo(f'start running {number} envs')
 
     session = server.new_session()
-    session.attached_pane.send_keys('echo Howdy', enter=True)
+    pane = session.attached_window.attached_pane
+    run_commands(pane)
 
     for i in range(number - 1):
         window = session.new_window(attach=False)
         pane = window.attached_pane
-        pane.send_keys('echo Howdy', enter=True)
+        run_commands(pane)
 
 
 @click.group()
@@ -30,14 +32,28 @@ def cli_2():
 
 
 @cli_2.command()
-@click.option('-s', '--session-name', required=True, type=click.IntRange(min=0),
-              help='Tmux-session number where environments are running')
+@click.option('-s', '--session-id', required=True, type=click.IntRange(min=0),
+              help='Tmux-session id where environments are running')
 @click.option('-n', '--number', required=True, type=click.IntRange(min=0),
               help='The sequence number of environment that can be killed')
-def stop(session_name, number):
+def stop(session_id, number):
     """Command on cli_2"""
     click.echo('Command stop on cli_2')
-    click.echo(f'stop env with number {number} at session {session_name}')
+    click.echo(f'stop env with number {number} at session {session_id}')
+
+    session = server.get_by_id(f'${session_id}')
+    found_pane = None
+
+    for window in session.list_windows():
+        for pane in window.list_panes():
+            if pane.get('pane_id')[1:] == str(number):
+                found_pane = pane
+                pane.window.kill_window()
+                break
+
+    if found_pane is None:
+        print('No environment with such id running')
+        return
 
 
 @click.group()
@@ -54,10 +70,28 @@ def stop_all(session_id):
     click.echo(f'find session by id {session_id} and stop it')
     session_to_stop = server.get_by_id(f'${session_id}')
     click.echo(f'{session_to_stop}')
-    # click.echo(f'stop all environments at session {session_id}')
+
+    os.system(f'tmux kill-session -t {session_id}')
+    click.echo(f'stop session {session_id}')
 
 
 cli = click.CommandCollection(sources=[cli_1, cli_2, cli_3])
+
+
+def run_commands(pane, base_dir='./', port=10916):
+    folder_num = pane.get('pane_id')[1:]
+    folder = base_dir + folder_num
+    pane.send_keys(f'mkdir {folder}')
+    pane.send_keys(f'cd {folder}')
+    env = f'venv_{folder_num}'
+    pane.send_keys(f'python3 -m venv {env}')
+    pane.send_keys(f'source {env}/bin/activate')
+
+    port += int(folder_num)
+    print(port)
+    token_ = os.urandom(24).hex()
+    pane.send_keys(f'jupyter notebook --ip=$(hostname -i) --port {port} --no-browser --NotebookApp.token="{token_}" \
+                    --NotebookApp.notebook_dir="{base_dir}"')
 
 
 if __name__ == '__main__':
