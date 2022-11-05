f648ab7c32d5870e16a5b3af5b6811e722f6fbc0
NataliaZvezdina <natalja.zvezdina@yandex.by>
2022-11-05 15:39:16 +0300

    add psycopg2 exception handling

--- docker/web/app.py
+++ docker/web/app.py
@@ -14,7 +14,6 @@ host = 'postgres_db'
 port = '5432'
 
 params = f'dbname={dbname} user={user} password={password} host={host} port={port}'
-print(params)
 
 
 @app.route('/', defaults={'path': ''}, methods=http_methods)
@@ -24,12 +23,18 @@ def catch_all(path):
         return '', 404
 
     if path == '':
-        con = psycopg2.connect(params)
-        cur = con.cursor(cursor_factory=RealDictCursor)
-        cur.execute('select * from test')
-        data = cur.fetchall()
-        con.close()
-        return data, 200
+        try:
+            con = psycopg2.connect(params)
+        except psycopg2.OperationalError as e:
+            app.logger.error(e)
+            return '', 404
+        else:
+            cur = con.cursor(cursor_factory=RealDictCursor)
+            cur.execute('select * from test')
+            data = cur.fetchall()
+            cur.close()
+            con.close()
+            return data, 200
     elif path == 'health':
         return {'status': 'OK'}, 200
     else:
